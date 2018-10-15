2004939d4c6b7851d72d3c73cd712ae2888de2a5
HafshahNada <42725368+HafshahNada@users.noreply.github.com>
2018-10-15 11:13:43 +0700

    Update search.php

--- search.php
+++ search.php
@@ -1,19 +1,19 @@
 <?php
+		include_once 'db.php';
 		echo("<H3>Hasil Pencarian</H3>");
-		mysql_connect("localhost","","");
-		mysql_select_db("TEST_DB");
+		$search = isset ($_POST['search'])? $_POST['search']: " ";
 
-		$perintah="SELECT * FROM datamaba WHERE nim like '%$cari'";
-		$hasil=mysql_query($perintah);
-		$jumlah=mysql_num_rows($hasil);
+		$perintah="SELECT * FROM datamaba WHERE nim like '%$search'";
+		$hasil=mysqli_query($conn,$perintah);
+		$jumlah=mysqli_num_rows($hasil);
 		if($jumlah>0){
-			echo "Data yang ditemukan : $jumlah<br><hr>");
-while($data=mysql_fetch_row($hasil)){
-	echo ("Nama : $data[1]<br>");
-	echo ("Nama : $data[1]<br>");
+			echo "Data yang ditemukan : $jumlah<br><hr>";
+while($data=mysqli_fetch_row($hasil)){
+	echo ("Nama : $data[0]<br>");
+	echo ("Nim : $data[1]<br>");
 
 }
 		}else{
-			echo "Maaf, Data yang anda cari tidak ada");
+			echo "Maaf, Data yang anda cari tidak ada";
 		}
 		?>
